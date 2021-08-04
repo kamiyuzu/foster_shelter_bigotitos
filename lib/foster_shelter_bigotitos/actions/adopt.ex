ef383e783a43b65e0a5e7fc163808a0492131188
arevueltaa <alberto.revuelta@coverwallet.com>
2021-08-04 22:22:11 +0200

    Move required modules into a contract

--- lib/foster_shelter_bigotitos/actions/adopt.ex
+++ lib/foster_shelter_bigotitos/actions/adopt.ex
@@ -2,22 +2,24 @@ defmodule FosterShelterBigotitos.Actions.Adopt do
 
   alias FosterShelterBigotitos.Repo
   alias FosterShelterBigotitos.Customers
+  alias FosterShelterBigotitos.Customers.Customer
   alias FosterShelterBigotitos.Animals
   alias FosterShelterBigotitos.Animals.Animal
 
-  def call(adopt_params) do
+  def call(adopt_params, opts \\ []) do
+    {animals_module, customers_module, repo_module} = required_modules(opts)
     {animal_id, update_attrs, customer_email} = parse_params(adopt_params)
-    animal = animal_id |> Animals.get_animal!() |> Repo.preload([:customer])
-    customer = Customers.get_customer_by_email(customer_email)
+    animal = animal_id |> animals_module.get_animal!() |> repo_module.preload([:customer])
+    customer = customers_module.get_customer_by_email(customer_email)
     animal_changeset = Ecto.Changeset.cast(%Animal{}, update_attrs, [:name, :species, :age])
 
     updated_animal =
       animal
       |> Ecto.Changeset.change(animal_changeset.changes)
       |> Ecto.Changeset.put_assoc(:customer, customer)
-      |> Repo.update()
+      |> repo_module.update()
 
-      with %Customers.Customer{} <- customer,
+      with %Customer{} <- customer,
          {:ok, %Animal{} = animal} <- updated_animal do
       animal
     end
@@ -26,5 +28,12 @@ defmodule FosterShelterBigotitos.Actions.Adopt do
   defp parse_params(adopt_params) do
     %{"id" => animal_id, "animal" => update_attrs, "customer_email" => customer_email} = adopt_params
     {animal_id, update_attrs, customer_email}
+  end
+
+  defp required_modules(opts) do
+    animals_module = Keyword.get(opts, :animals, Animals)
+    customers_module = Keyword.get(opts, :customers, Customers)
+    repo_module = Keyword.get(opts, :repo, Repo)
+    {animals_module, customers_module, repo_module}
   end
 end
